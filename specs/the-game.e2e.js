68ae6438de3d9c554d2423503d0eb909f8fe135a
maxve1 <emvi@tut.by>
2021-12-16 21:19:37 +0300

    added arkanoid game

--- specs/the-game.e2e.js
+++ specs/the-game.e2e.js
@@ -1,13 +1,21 @@
 describe('The game', function () {
     it('should get more than 100 points ', async function () {
-        await browser.url('http://localhost:63342/course-sut/arkanoid.html');
+        await browser.url('https://viktor-silakov.github.io/course-sut/arkanoid.html');
         await browser.pause(1000);
         await $('button=PLAY').click();
         await browser.waitUntil(async () => {
             // player logic
+            const ballCoordinates = await $('#ball').getLocation('x');
+            const padCoordinates = await $('#pad').getLocation('x');
+      
+            if(ballCoordinates < padCoordinates){
+                await browser.keys('A');
+            } else if(ballCoordinates > padCoordinates ){
+                await browser.keys('D');
+            }
             const points = parseInt(await $('#points').getText(), 10);
             if (points > 100) return true
             console.log({ points })
-        }, { timeout: 600000, interval: 10 })
+        }, { timeout: 60000, interval: 10 })
     });
 })
